9490ae2cf9b7f93dde191abb4f75a3bc40c71407
vik-ivahnuk <vik.ivahnuk@gmail.com>
2022-05-02 09:51:53 +0300

    lab 5 refactor

--- lab5/main.py
+++ lab5/main.py
@@ -1,6 +1,8 @@
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
-channel.queue_declare(queue='to_agency')
-channel.queue_declare(queue='from_agency')
+if __name__ == '__main__':
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='to_agency')
+    channel.queue_declare(queue='from_agency')
+    print('queues created successfully')
