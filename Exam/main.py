2b231c423d6d7a6c5ad0b5e410c02880e3e9f222
vik-ivahnuk <vik.ivahnuk@gmail.com>
2022-06-01 13:11:44 +0300

    fix

--- Exam/main.py
+++ Exam/main.py
@@ -9,12 +9,14 @@ class Concert:
         self.price = price
         self.authors = authors
 
-
+    def __repr__(self):
+        return "place {0} genre {1} data {2} price {3} authors".format(self.place, self.genre,
+                                                                       self.data, self.price, self.authors)
 
 
 class VirtualConcert(Concert):
     def __init__(self, place, genre, data, price, authors):
-        super.__init__(place, genre, data, price, authors)
+        super().__init__(place, genre, data, price, authors)
 
     def ticket_booking(self):
         return True  # всігда бесплатно
@@ -22,7 +24,7 @@ class VirtualConcert(Concert):
 
 class LiveConcert(Concert):
     def __init__(self, place, genre, data, price, authors, num_tikets):
-        super.__init__(place, genre, data, price, authors)
+        super().__init__(place, genre, data, price, authors)
         self.num_tikets = num_tikets
 
     def ticket_booking(self):
@@ -38,10 +40,34 @@ class Manager:
 
     def tiket_booking(self, place, live: bool):
         for a in self.concerts:
-            if (isinstance(a,LiveConcert) and place == a.place) or live == True:
+            if (isinstance(a, LiveConcert) and place == a.place) or live :
                 return a.ticket_booking()
         return False
 
-    def sort_by_name(self):
-        self.concerts.sort(key=lambda x: x.name)
+    def sort_by_price(self):
+        self.concerts.sort(key=lambda x: x.price)
 
+    def get_concert_by_genre(self, genre):
+        res = list()
+        for a in self.concerts:
+            if a.genre == genre:
+                res.append(a)
+        return res
+
+
+
+if __name__ == "__main__":
+    manager = Manager()
+    manager.concerts= [
+        VirtualConcert("kiyv", "vocal", "11.11.22", 11, "jamala"),
+        VirtualConcert("kiyv", "vocal", "12.11.22", 14, "noize"),
+        LiveConcert("New York", "vocal", "12.11.22", 41, "noize", 44),
+        LiveConcert("New York", "danse", "12.11.22", 11, "noize", 44),
+    ]
+
+    print(manager.tiket_booking("New York", False))
+    manager.sort_by_price()
+    for a in manager.concerts:
+        print(a)
+    print("#"*20,"\n")
+    print(manager.get_concert_by_genre("vocal"))
